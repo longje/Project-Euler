5277276459ef9bd983941bd20118016255e6a236
longje <longje@solvebysearch.com>
2012-08-05 18:04:47 -0400

    Finished additional problems

---
+++ Graphs/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphs
+{
+    public class Edge
+    {
+        public int weight;
+        public Node u;
+        public Node v;
+
+        public Edge(int w, Node u, Node v)
+        {
+            this.weight = w;
+            this.u = u;
+            this.v = v;
+        }
+    }
+}
---
+++ Graphs/Graph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphs
+{
+    class Graph
+    {
+        public int numberOfNodes;
+        public int numberOfEdges;
+        public Edge[] edges;
+        public Node[] nodes;
+
+        public Graph(Edge[] edges, Node[] nodes)
+        {
+            this.edges = edges;
+            this.nodes = nodes;
+        }
+    }
+}
