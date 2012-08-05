1b3f035d2a9d4d2003cefd0988fedd0710bf0e63
longje <longje@solvebysearch.com>
2012-08-05 18:26:23 -0400

    Removed unecessary notes

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -40,63 +40,44 @@ namespace ProjectEuler
 					new Problem23(),
 					new Problem24(),
 					new Problem25(),
-                    new Problem27(),
+				    	new Problem27(),
 					new Problem28(),
 					new Problem29(),
 					new Problem30(),
-                    new Problem31(),
-                    new Problem32(),
-                    new Problem33(),
+				    	new Problem31(),
+				    	new Problem32(),
+				    	new Problem33(),
 					new Problem34(),
 					new Problem35(),
 					new Problem36(),
 					new Problem37(),
-			        new Problem38(),
-			        new Problem39(),
-                    new Problem41(),
+			        	new Problem38(),
+			        	new Problem39(),
+                    			new Problem41(),
 					new Problem42(),
-			        new Problem43(),
-			        new Problem44(),
+					new Problem43(),
+					new Problem44(),
 					new Problem45(),
-                    new Problem46(),
-                    new Problem47(),
-			        new Problem48(),
-                    new Problem49(),
-                    new Problem50(),
-                    new Problem52(),
-                    new Problem53(),
-                    new Problem55(),
-                    new Problem56(),
-                    new Problem59(),
-                    new Problem63(),
+				    	new Problem46(),
+				    	new Problem47(),
+					new Problem48(),
+					new Problem49(),
+					new Problem50(),
+					new Problem52(),
+					new Problem53(),
+					new Problem55(),
+					new Problem56(),
+					new Problem59(),
+					new Problem63(),
 					new Problem67(),
-                    new Problem79(),
-                    new Problem97()
+				    	new Problem79(),
+			    		new Problem97()
 				};
 
 			Stopwatch watch = new Stopwatch();
 			watch.Start();
 
 <<<<<<< HEAD
-            /* Amortization for loan */
-            /*
-            for (double n = 2; n < 360; n++)
-            {
-                double p = 300000;
-                double r = 0.0399;
-                double t = 1;
-                //double n = 5;
-                double result = (p * (r / n)) / (1 - Math.Pow((1 + r / n), -n * t));
-                double interestPaid = (p * r) / 12;
-                for (int i = 1; i < n; i++)
-                {
-                    interestPaid += ((p - result) * r) / 12;
-                    p -= result;
-                }
-                Console.WriteLine("For {2} payments, you're monthly payment is: {0} and you will the total interest paid is {1}", result, interestPaid, n);
-            }
-            */
-=======
 >>>>>>> 026c19f614b0b3c3fafa634960833e640c2ee0c2
             //new Problem92().Solve();
             new Problem57().Solve();
@@ -106,22 +87,6 @@ namespace ProjectEuler
 			Console.WriteLine("Time: {0}", watch.ElapsedMilliseconds);
             Console.WriteLine();
 <<<<<<< HEAD
-			//new Problem48().Solve();
-
-			//Problem 48
-			//Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
-			//Answer - 9110846700
-
-			//var query = from x in Enumerable.Range(1, 500)
-			//            select (Math.Pow(x, x));
-
-			//var result = new BigInteger(0);
-			//foreach (var item in query)
-			//{
-			//    result += item;
-			//}
-
-			//Console.WriteLine(result);
 =======
 
 >>>>>>> 026c19f614b0b3c3fafa634960833e640c2ee0c2
